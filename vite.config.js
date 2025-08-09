1f4bd337a6ce598158ed79800648912c734545b3
jres <ljresetl@gmail.com>
2025-08-09 18:24:29 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/goit-js-hw-10/',
+    base: '/goit-js-hw-11/',
     build: {
       sourcemap: true,
       rollupOptions: {
